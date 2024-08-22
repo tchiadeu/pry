4903c85e427cc7627e0c6752abab129b0b09deb2
Kévin TCHIADEU <122535041+tchiadeu@users.noreply.github.com>
2024-08-22 19:13:02 +0200

    feature: add git branch

--- pry.rb
+++ pry.rb
@@ -1,19 +1,26 @@
 # frozen_string_literal: true
 
+def current_git_branch
+  branch = `git rev-parse --abbrev-ref HEAD 2>/dev/null`.chomp
+  $CHILD_STATUS.exitstatus.zero? ? branch : 'no-git'
+end
+
 if defined?(Rails)
-  short_env_name = {
-    'development' => { name: 'dev', color: :green },
-    'production' => { name: 'prod', color: :red },
-    'staging' => { name: 'stage', color: :green },
-    'test' => { name: 'test', color: :green }
+  short_env_options = {
+    'development' => 'dev',
+    'production' => 'prod',
+    'staging' => 'stage'
   }
   app_name = Rails.application.class.module_parent_name.underscore.dasherize
-  env_options = short_env_name[Rails.env]
-  env = Pry::Helpers::Text.send(env_options[:color], env_options[:name])
-  description = 'Prompt has to match de rails app name'
+  env_color = Rails.env.production? ? :red : :green
+  short_env_name = short_env_options[Rails.env]
+  env_and_git = "#{short_env_name}/#{current_git_branch}"
+  env_name = Rails.env.development? ? env_and_git : short_env_name
+  env = Pry::Helpers::Text.send(env_color, env_name)
+  description = 'Prompt has to match the rails app name'
 else
   app_name = Dir.pwd.split('/').last.to_s
-  env = Pry::Helpers::Text.green('main')
+  env = Pry::Helpers::Text.green(current_git_branch)
   description = 'Prompt has to match the current folder name'
 end
 
@@ -23,8 +30,8 @@ Pry::Prompt.add(:current_app) do |_context, nesting, pry_instance, sep|
   format(
     '[%<in_count>s] %<app>s(%<env>s)%<nesting>s%<separator>s ',
     in_count: pry_instance.input_ring.count,
-    app: app,
-    env: env,
+    app:,
+    env:,
     nesting: (nesting.positive? ? ":#{nesting}" : ''),
     separator: sep
   )
@@ -37,5 +44,5 @@ procs = [
 ]
 
 Pry.config.prompt = Pry::Prompt.new(
-  'custom_prompt', description, procs
+  'custom_app_prompt', description, procs
 )
